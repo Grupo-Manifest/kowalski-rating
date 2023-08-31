bba3b61c299aa3b903d78db0f44c3b8e589dd4b3
Kaue Miziara <kauemiziara@gmail.com>
2023-08-31 18:19:53 -0300

    Create ReviewDialog to let the user create a new review

---
+++ app/src/main/java/ecb/manifest/kowalski/rating/ui/presentation/review/ReviewDialog.kt
@@ -0,0 +1,41 @@
+package ecb.manifest.kowalski.rating.ui.presentation.review
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import ecb.manifest.kowalski.rating.events.ReviewEvent
+import ecb.manifest.kowalski.rating.events.ReviewState
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddReviewDialog(
+    state: ReviewState,
+    onEvent: (ReviewEvent) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    AlertDialog(
+        modifier = modifier,
+        onDismissRequest = { onEvent(ReviewEvent.HideReviewDialog) },
+        title = { Text(text = "Add review") },
+        text = {
+            Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
+                TextField(
+                    value = state.serviceQuality.toString(),
+                    onValueChange = {
+                        onEvent(ReviewEvent.SetServiceQuality(1))
+                    },
+                    placeholder = { Text(text = "Service Qaulity") }
+                )
+            }
+        },
+        confirmButton = {
+            onEvent(ReviewEvent.SaveReview)
+        },
+    )
+}
