42ec18232f7b4b43e88540e02efd90dfa22c1b9c
Kaue Miziara <kauemiziara@gmail.com>
2023-09-10 14:59:18 -0300

    Create Navigation composable to handle navigation

--- app/src/main/java/ecb/manifest/kowalski/rating/ui/main/MainActivity.kt
+++ app/src/main/java/ecb/manifest/kowalski/rating/ui/main/MainActivity.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import dagger.hilt.android.AndroidEntryPoint
+import ecb.manifest.kowalski.rating.ui.navigation.Navigation
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainScreen()
+            Navigation()
         }
     }
 }
---
+++ app/src/main/java/ecb/manifest/kowalski/rating/ui/navigation/Navigation.kt
@@ -0,0 +1,25 @@
+package ecb.manifest.kowalski.rating.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import ecb.manifest.kowalski.rating.ui.main.MainScreen
+import ecb.manifest.kowalski.rating.ui.presentation.review.ReviewScreen
+import ecb.manifest.kowalski.rating.ui.viewModels.ReviewViewModel
+
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "main_screen") {
+        composable(Screen.MainScreen.route) {
+            val viewModel = hiltViewModel<ReviewViewModel>()
+            MainScreen(viewModel, navController)
+        }
+        composable(Screen.ReviewScreen.route) {
+            val viewModel = hiltViewModel<ReviewViewModel>()
+            ReviewScreen(viewModel)
+        }
+    }
+}
