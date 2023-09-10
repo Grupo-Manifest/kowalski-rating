05e59ae6c611f21cd1d1b2f24a7d9ada064e7d98
RodazTheCoder <rodrigofhc41@gmail.com>
2023-09-09 22:35:52 -0300

    Add initial main screen UI

--- app/src/main/java/ecb/manifest/kowalski/rating/ui/main/MainActivity.kt
+++ app/src/main/java/ecb/manifest/kowalski/rating/ui/main/MainActivity.kt
@@ -3,23 +3,56 @@ package ecb.manifest.kowalski.rating.ui.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import dagger.hilt.android.AndroidEntryPoint
 import ecb.manifest.kowalski.rating.ui.presentation.review.ReviewScreen
-import ecb.manifest.kowalski.rating.ui.theme.RatingTheme
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ReviewScreen()
+            Column(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .weight(1.5f)
+                        .background(color = Color.Yellow)
+                ) {
+                    Column(modifier = Modifier.padding(7.dp)){
+                        Text(
+                            text = "AVALIAÇÃO",
+                            modifier = Modifier.fillMaxWidth(),
+                            textAlign = TextAlign.Center,
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 30.sp,
+                        )
+                    }
+                }
+
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .weight(4f)
+                        .background(color = Color.White)
+                ) {
+                    ReviewScreen()
+                }
+            }
         }
     }
 }
